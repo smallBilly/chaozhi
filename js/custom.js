b3b9f0a47573884bf05aecabe1e05189214ae28c
smallBilly <2322357841@qq.com>
2018-01-05 14:02:33 +0800

    提交修改

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+$(document).ready( function() {
+    // sidebar menu click
+    $('.templatemo-sidebar-menu li.sub a').click(function(){
+        if($(this).parent().hasClass('open')) {
+            $(this).parent().removeClass('open');
+        } else {
+            $(this).parent().addClass('open');
+        }
+    });
+
+});
